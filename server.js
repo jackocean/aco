b22eb37505065c49f2f261f7bf0583a6c967316c
杨帆 <jackyangfan@corp.netease.com>
2015-07-17 17:04:10 +0800

    MISC:加debugger 用 node-inspector 调试

--- server.js
+++ server.js
@@ -3,5 +3,7 @@ var http = require("http");
 http.createServer(function(request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.write("Hello World");
+  debugger
+  console.log('debugging');
   response.end();
 }).listen(8888);
