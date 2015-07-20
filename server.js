02c22f8eaed43cc638844888cb7b88c6e499a1e1
杨帆 <jackyangfan@corp.netease.com>
2015-07-20 16:56:52 +0800

    MISC:nodejs 入门 demo (非阻塞)

--- server.js
+++ server.js
@@ -3,14 +3,15 @@ var url = require("url");
 
 function start(route, handle){
 	function onRequest(request, response) {
-		debugger
 		var pathname = url.parse(request.url).pathname;
 		console.log("Request for " + pathname + " received.");
 		
-		response.writeHead(200, {"Content-Type": "text/plain"});
-		var content = route(pathname, handle)
-	    response.write(content);
-	    response.end();
+		route(pathname, handle, response);
+		
+//		response.writeHead(200, {"Content-Type": "text/plain"});
+//		var content = route(pathname, handle);
+//	    response.write(content);
+//	    response.end();
 	}
 	
 	http.createServer(onRequest).listen(8888);
